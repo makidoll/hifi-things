398f8fff9200a20eae27d24364c8e125fa897ab3
Maki <mxmcube@gmail.com>
2018-12-20 11:53:44 -0800

    Work in progress app

---
+++ client-scripts/hifiEssentials/app/content.js
@@ -0,0 +1,173 @@
+var content = {
+	avatars: [
+		{
+			title: "<img src='avatars/mgf.png' style='height: 80px; margin-bottom: -24px;'/>",
+			marginBottom: "32px",
+			avatars: [
+				{
+					name: "Akari Akaza",
+					thumbnail: "avatars/mgf/yraitm001.jpg",
+					url: "https://maki.cat/hifi/mgf/avatars/yraitm001.fst",
+				},
+				{
+					name: "Chinatsu Yoshikawa",
+					thumbnail: "avatars/mgf/yrcitm001.jpg",
+					url: "https://maki.cat/hifi/mgf/avatars/yrcitm001.fst",
+				},
+				{
+					name: "Kyouko Toshinou",
+					thumbnail: "avatars/mgf/yrkitm001.jpg",
+					url: "https://maki.cat/hifi/mgf/avatars/yrkitm001.fst",
+				},
+				{
+					name: "Yui Funami",
+					thumbnail: "avatars/mgf/yryitm001.jpg",
+					url: "https://maki.cat/hifi/mgf/avatars/yryitm001.fst",
+				},
+				{
+					name: "Akari Akaza",
+					thumbnail: "avatars/mgf/yraitm002.jpg",
+					url: "https://maki.cat/hifi/mgf/avatars/yraitm002.fst",
+				},
+				{
+					name: "Chinatsu Yoshikawa",
+					thumbnail: "avatars/mgf/yrcitm002.jpg",
+					url: "https://maki.cat/hifi/mgf/avatars/yrcitm002.fst",
+				},
+				{
+					name: "Kyouko Toshinou",
+					thumbnail: "avatars/mgf/yrkitm002.jpg",
+					url: "https://maki.cat/hifi/mgf/avatars/yrkitm002.fst",
+				},
+				{
+					name: "Yui Funami",
+					thumbnail: "avatars/mgf/yryitm002.jpg",
+					url: "https://maki.cat/hifi/mgf/avatars/yryitm002.fst",
+				},
+			],
+		},
+		{
+			title: "<img src='avatars/subcom.svg' style='height: 40px; margin-bottom: -8px;'/>",
+			marginBottom: "0",
+			avatars: [
+				{
+					name: "Daven Atlanta",
+					thumbnail: "avatars/subcom/daven-atlanta.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/LittleMouse/HF%20Daven%20Atlanta%20Atlas/HF%20Daven%20Atlanta%20Atlas.fst"
+				},
+				{
+					name: "Witch",
+					thumbnail: "avatars/subcom/witch.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/LittleMouse/HF%20-%20Witch/HF%20-%20Witch.fst"
+				},
+				{
+					name: "Gangstar Sleeveless",
+					thumbnail: "avatars/subcom/gangstar-sleeveless.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/LittleMouse/HF%20-%20Gangstar%20Sleeveless%20Finger%20Fix/HF%20-%20Gangstar%20Sleeveless%20Finger%20Fix.fst"
+				},
+				{
+					name: "Wolfy",
+					thumbnail: "avatars/subcom/wolfy.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/Diviy/Working%20exports%20only/wolfy2/wolfy2.fst"
+				},
+				{
+					name: "Cute Converse",
+					thumbnail: "avatars/subcom/cute-converse.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/Diviy/Working%20exports%20only/cute%20convers4/cute%20convers4.fst"
+				},
+				{
+					name: "Wolf Trap Tanooki",
+					thumbnail: "avatars/subcom/wolf-trap-tanooki.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/LittleMouse/HF%20Wolf%20Trap%20-%20Tanooki%20atlas%20-%20weight%20painting%20fix/HF%20Wolf%20Trap%20-%20Tanooki%20atlas%20-%20weight%20painting%20fix.fst"
+				},
+				{
+					name: "Emo Wolf Boy",
+					thumbnail: "avatars/subcom/emo-wolf-boy.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/LittleMouse/HF%20Emo%20Wolf%20Boy%20Finger%20Fixed/HF%20Emo%20Wolf%20Boy%20Finger%20Fixed.fst"
+				},
+				{
+					name: "Goth Ooka",
+					thumbnail: "avatars/subcom/goth-ooka.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/Diviy/Working%20exports%20only/goth%20ooka6/goth%20ooka6.fst"
+				},
+				{
+					name: "Teen Neko",
+					thumbnail: "avatars/subcom/teen-neko.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/Diviy/Working%20exports%20only/teen%20neko5/teen%20neko5.fst"
+				},
+				{
+					name: "Headphone Wolfy",
+					thumbnail: "avatars/subcom/headphone-wolfy.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/Diviy/Working%20exports%20only/headphone%20wolfy%207/headphone%20wolfy%207.fst"
+				},
+				{
+					name: "Azuma Lim",
+					thumbnail: "avatars/subcom/azuma-lim.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/LittleMouse/Azuma%20Lim%20edit%20by%20Subcom%20Finger%20Edit/Azuma%20Lim%20edit%20by%20Subcom%20Finger%20Edit.fst"
+				},
+				{
+					name: "Timbs Hoppou",
+					thumbnail: "avatars/subcom/timbs-hoppou.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/Diviy/Working%20exports%20only/Timbs%20hoppou5/Timbs%20hoppou4.fst"
+				},
+				{
+					name: "Sweater Dress",
+					thumbnail: "avatars/subcom/sweater-dress.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/Diviy/Working%20exports%20only/sweater%20dress10/sweater%20dress10.fst"
+				},
+				{
+					name: "Lilith",
+					thumbnail: "avatars/subcom/lilith.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/LittleMouse/HF%20-%20Lilith%202%20fixed%20emit%20transparency%20NO%20PSD/HF%20-%20Lilith%202%20fixed%20emit%20transparency%20NO%20PSD.fst"
+				},
+				{
+					name: "Luna Fox",
+					thumbnail: "avatars/subcom/luna-fox.jpg",
+					url: "https://hifi-makers.s3.amazonaws.com/subcom/LittleMouse/HF%20Luna%20Fox%20EM%20FINAL/HF%20Luna%20Fox%20EM%20FINAL.fst"
+				},
+			]
+		},
+		{
+			title: "Others",
+			marginBottom: "0",
+			avatars: []
+		},
+	],
+}
+
+// avatars
+content.avatars.forEach(category=>{
+	let div = document.createElement("div");
+
+	category.avatars.forEach(avatar=>{
+		let avatarEl = document.createElement("a");
+		avatarEl.href = "javascript:";
+		avatarEl.addEventListener("click", e=>{
+			changeAvatar(avatar.name, avatar.url);
+		});
+
+		let avatarSelector = document.createElement("div");
+		avatarSelector.id = "avatar-selector";
+
+		let avatarThumbnail = document.createElement("div");
+		avatarThumbnail.id = "avatar-thumbnail";
+		avatarThumbnail.style.backgroundImage = "url("+avatar.thumbnail+")";
+
+		let avatarName = document.createElement("p");
+		avatarName.id = "avatar-name";
+		avatarName.innerHTML = avatar.name;
+
+		avatarSelector.appendChild(avatarThumbnail);
+		avatarSelector.appendChild(avatarName);
+		avatarEl.appendChild(avatarSelector);
+		div.appendChild(avatarEl);
+	});
+
+	let marginBottom = "0";
+	if (category.marginBottom)
+		marginBottom = category.marginBottom;
+
+	getPageEl("avatars").appendChild(new Collapse(category.title, div, {
+		marginBottom: marginBottom
+	}))
+}) 
