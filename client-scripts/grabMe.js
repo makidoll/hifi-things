fe31327f32c1b4bd4f1e7a56fe87a1e5d0cce0a9
Maki <mxmcube@gmail.com>
2018-12-31 21:07:52 -0800

    idk...

--- client-scripts/grabMe.js
+++ client-scripts/grabMe.js
@@ -1,12 +1,31 @@
 function GrabMe() {
 	var entityID = undefined;
-	var active = false;
+	this.active = false;
 	var interval = undefined;
 	var previousScale = 1;
+// 	var isTabletDisplayed = false;
+// 	var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+
+// 	function onTabletScreenChanged(type, url) {
+//         isTabletDisplayed = type !== "Closed";
+//         updateHandFunctions();
+//     }
+
+// 	function updateHandFunctions() {
+//         Messages.sendMessage("Hifi-Grab-Disable", JSON.stringify({
+//             holdEnabled: !active,
+//             nearGrabEnabled: !active,
+//             farGrabEnabled: !active
+//         }), true);
+// 1
+//         Messages.sendMessage("Hifi-Pointer-Disable", JSON.stringify({
+//             pointerEnabled: isTabletDisplayed
+//         }), true);
+//     }
 
 	this.enable = function() {
-		if (active) return;
-		if (!Entities.canRez() && !Entities.canRezTmp()) return;
+		if (this.active) return;
+		if (!(Entities.canRez() || Entities.canRezTmp())) return;
 
 		entityID = Entities.addEntity({
 			name: "Grab Me - "+MyAvatar.sessionDisplayName,
@@ -27,9 +46,20 @@ function GrabMe() {
 			rotation: MyAvatar.orientation,
 		});
 		if (!entityID) return;
+		this.active = true;
+
+		//tablet.screenChanged.connect(onTabletScreenChanged);
+		//updateHandFunctions();
+		Messages.sendMessage("Hifi-Hand-Drop", "both");
+		Messages.sendMessage("Hifi-Hand-Drop", "both");
+		Messages.sendMessage("Hifi-Hand-Drop", "both");
+		Messages.sendMessage("Hifi-Hand-Drop", "both");
+		Messages.sendMessage("Hifi-Hand-Drop", "both");
+
+		previousScale = MyAvatar.scale;
+		MyAvatar.scale = 0.274;
 
 		Controller.disableMapping("com.highfidelity.controllerDispatcher");
-		MyAvatar.setParentID(entityID);
 		MyAvatar.setCollisionsEnabled(false);
 
 		interval = Script.setInterval(function() {
@@ -46,31 +76,29 @@ function GrabMe() {
 			//MyAvatar.orientation = entity.rotation; 
 		}, 1000);
 
-		previousScale = MyAvatar.scale;
-		MyAvatar.scale = 0.274;
-
-		active = true;
+		MyAvatar.setParentID(entityID);
 	}
 
 	this.disable = function() {
-		if (!active) return;
+		if (!this.active) return;
+		this.active = false;
 
 		Entities.deleteEntity(entityID);
 
-		Controller.enableMapping("com.highfidelity.controllerDispatcher");
-		MyAvatar.setParentID("");
-		MyAvatar.setCollisionsEnabled(true);
-		
-		if (interval) Script.clearInterval(interval);
+		//tablet.screenChanged.disconnect(onTabletScreenChanged);
+		//updateHandFunctions();
 
 		MyAvatar.scale = previousScale;
 		MyAvatar.orientation = Quat.cancelOutRollAndPitch(MyAvatar.orientation);
+		
+		Controller.enableMapping("com.highfidelity.controllerDispatcher");
+		MyAvatar.setCollisionsEnabled(true);
 
-		active = false;
+		if (interval) Script.clearInterval(interval);
+
+		MyAvatar.setParentID("");
 	}
 }
-
-var grabMe = new GrabMe();
 
 var icons = {
 	white: 'data:image/svg;xml,<svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.5 6c-2.61.7-5.67 1-8.5 1s-5.89-.3-8.5-1L3 8c1.86.5 4 .83 6 1v13h2v-6h2v6h2V9c2-.17 4.14-.5 6-1l-.5-2zM12 6c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"/></svg>',
@@ -83,18 +111,21 @@ var button = tablet.addButton({
 	text: "Grab Me"
 });
 
-function clicked() {
-	grabMe.active = !grabMe.active;
-	button.editProperties({
-		isActive: grabMe.active,
-		icon: (grabMe.active)? icons.black: icons.white
-	});
+var grabMe = new GrabMe();
 
-	if (grabMe.active) {
+function clicked() {
+	var newActive = !grabMe.active;
+
+	if (newActive) {
 		grabMe.enable();
 	} else {
 		grabMe.disable();
 	}
+
+	button.editProperties({
+		isActive: newActive,
+		icon: (newActive)? icons.black: icons.white
+	});
 }
 
 button.clicked.connect(clicked);
