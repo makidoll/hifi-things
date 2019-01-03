1a7f58c567bc5f7d979f5d5d2bc613727751aefb
Maki <mxmcube@gmail.com>
2019-01-02 20:25:09 -0800

    Dist updates

--- client-scripts/hifiEssentials/dist/shader.fs
+++ client-scripts/hifiEssentials/dist/shader.fs
@@ -20,6 +20,7 @@
 #define animWidth 4
 #define animHeight 8
 #define animFPS 16
+#define bg vec3(29,31,33)/(255*8)
 
 vec4 _permute(vec4 x){return mod((x*34.+1.)*x,289.);}vec4 _taylorInvSqrt(vec4 r){return 1.79284-.853735*r;}float _snoise(vec3 v){const vec2 C=vec2(1./6.,1./3.);const vec4 D=vec4(0.,.5,1.,2.);vec3 i=floor(v+dot(v,C.yyy)),x0=v-i+dot(i,C.xxx),g=step(x0.yzx,x0.xyz),l=1.-g,i1=min(g.xyz,l.zxy),i2=max(g.xyz,l.zxy),x1=x0-i1+C.xxx,x2=x0-i2+2.*C.xxx,x3=x0-1.+3.*C.xxx;i=mod(i,289.);vec4 p=_permute(_permute(_permute(i.z+vec4(0.,i1.z,i2.z,1.))+i.y+vec4(0.,i1.y,i2.y,1.))+i.x+vec4(0.,i1.x,i2.x,1.));float n_=1./7.;vec3 ns=n_*D.wyz-D.xzx;vec4 j=p-49.*floor(p*ns.z*ns.z),x_=floor(j*ns.z),y_=floor(j-7.*x_),x=x_*ns.x+ns.yyyy,y=y_*ns.x+ns.yyyy,h=1.-abs(x)-abs(y),b0=vec4(x.xy,y.xy),b1=vec4(x.zw,y.zw),s0=floor(b0)*2.+1.,s1=floor(b1)*2.+1.,sh=-step(h,vec4(0.)),a0=b0.xzyw+s0.xzyw*sh.xxyy,a1=b1.xzyw+s1.xzyw*sh.zzww;vec3 p0=vec3(a0.xy,h.x),p1=vec3(a0.zw,h.y),p2=vec3(a1.xy,h.z),p3=vec3(a1.zw,h.w);vec4 norm=_taylorInvSqrt(vec4(dot(p0,p0),dot(p1,p1),dot(p2,p2),dot(p3,p3)));p0*=norm.x;p1*=norm.y;p2*=norm.z;p3*=norm.w;vec4 m=max(.6-vec4(dot(x0,x0),dot(x1,x1),dot(x2,x2),dot(x3,x3)),0.);m=m*m;return 42.*dot(m*m,vec4(dot(p0,x0),dot(p1,x1),dot(p2,x2),dot(p3,x3)));}
 
@@ -48,22 +49,27 @@ float getProceduralColors(inout vec3 diffuse, inout vec3 specular, inout float s
 
 	// get color
 	vec2 cUv = (vec2(-uv.x,-uv.y)+1) * 0.5; // 0 to 1 from top left
-	cUv.x = cUv.x*0.45 + 0.08; // shift uv to correct for image
+	cUv.x = cUv.x*0.7 + 0; // shift uv to correct for image
 	vec3 color = getAnimationColor(cUv);
-	color = mix(color, vec3(29,31,33)/255, 0.4);
+
+	if (uv.y>0) {
+		color = mix(color, bg, 0.4);
+	} else {
+		color = mix(color, bg, 0.9);
+	}
 
 	// noise allowance
-	vec2 nUv = 1-abs(uv*2) * 0.9; // 0 to 1 to 0
+	vec2 nUv = 1-abs(uv*2) * 1; // 0 to 1 to 0
 	float nA = min(nUv.x, nUv.y);
 
 	// noise
 	float n = (snoise(vec3(
-		uv.x*4, uv.y*6,
+		uv.x*6, uv.y*6,
 		iGlobalTime*0.2
 	))+1) / 2;
 	
 	if (nA<0) {
-		n += nA*1.2;
+		n += nA*1;
 		if (n<0) discard;
 	};
 
