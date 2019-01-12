c28825e030542caf520aec08d4e301dd69371bae
Maki <mxmcube@gmail.com>
2019-01-12 11:24:59 -0800

    Panorama

---
+++ shaders/panorama.fs
@@ -0,0 +1,43 @@
+// {
+// 	"ProceduralEntity": {
+// 		"shaderUrl": "https://hifi.maki.cat/shaders/panorama.fs",
+// 		"version": 2,
+// 		"channels": [
+// 			"https://maki.cat/hifi/models/makis-room2.jpg"
+// 		],
+// 		"grabbableKey": {
+// 			"grabbable": false
+// 		}
+// 	}
+// }
+
+const float PI = 3.14159265359;
+const float TAU = 6.28318530718;
+
+// https://makc3d.wordpress.com/2017/01/19/sampling-equirectangular-textures/
+vec3 getSkyboxImageColor(vec3 dir) {
+	float r = length(dir);
+	float theta = acos(-dir.y/r);
+	float phi = atan(dir.x, -dir.z);
+
+	return texture(iChannel0, vec2(
+		phi/TAU,
+		1-theta/PI
+   )).rgb;
+}
+
+// https://github.com/theepicsnail/hifi/tree/master/shaders
+float getProceduralColors(inout vec3 diffuse, inout vec3 specular, inout float shininess) {
+   	vec3 worldEye = getEyeWorldPos();
+	vec3 localPos = _position.xyz;
+   	vec3 worldPos = (iWorldOrientation*(localPos*iWorldScale)) + iWorldPosition;
+	//vec3 rayOrigin = getEyeWorldPos();
+	vec3 rayDir = normalize(worldPos-worldEye);
+
+	vec3 color = getSkyboxImageColor(rayDir);
+
+	diffuse = color;
+	specular = vec3(0);
+	shininess = 0.5;
+	return 1;
+}
