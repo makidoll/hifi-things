ed994bf482cd03ad7adf439122efed653882a576
Maki <mxmcube@gmail.com>
2019-01-11 19:26:29 -0800

    hifiEssentials installer

---
+++ client-scripts/hifiEssentials/dist/install.js
@@ -0,0 +1,46 @@
+(function() {
+	var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+	var scriptURL = Script.resolvePath("../hifiEssentials.js");
+	var installURL = Script.resolvePath("install.html");
+
+	function isInstalled() {
+		var filename = scriptURL.split("/").pop();
+		var runningScripts = ScriptDiscoveryService.getRunning();
+
+		for (var i=0; i<runningScripts.length; i++) {
+			if (runningScripts[i].name == filename) return true;
+		}
+
+		return false;
+	}
+
+	function webEventReceived(string) {
+		switch (string.replace("hifiEssentialsInstall-","")) {
+			case "install":
+				if (!isInstalled())
+					ScriptDiscoveryService.loadScript(scriptURL);
+			break;
+			case "close":
+				tablet.gotoHomeScreen();
+			break;
+		}
+	}
+
+	this.preload = function() {
+		tablet.webEventReceived.connect(webEventReceived);
+	}
+
+	this.unload = function () {
+		tablet.webEventReceived.disconnect(webEventReceived);
+	}
+
+	this.openInstall = function() {
+		tablet.gotoWebScreen(installURL);
+	}
+
+	this.startNearTrigger = function() { this.openInstall() };
+	this.startFarTrigger = function() { this.openInstall() };
+	this.clickDownOnEntity = function(entityID, mouseEvent) {
+		if (mouseEvent.isLeftButton) this.openInstall();
+	}
+})
