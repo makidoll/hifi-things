613184104e8c94605a1206a7c262da7ad4fb7986
Maki <mxmcube@gmail.com>
2019-01-26 17:23:12 -0800

    Updated teleport

--- client-entity-scripts/teleport.js
+++ client-entity-scripts/teleport.js
@@ -1,7 +1,6 @@
 /*
 {
-	"position": {"x":0,"y":0,"z":0},
-	"rotation": 0 // deg
+	"address": "" // you can find this in "Navigage > Copy Address to Clipboard"
 }
 */
 
@@ -19,14 +18,17 @@
 		try { var userData = JSON.parse(entity.userData);
 		} catch(err) { return; }
 
-		if (!userData.position) return;
-		var position = userData.position;
-		var rotation = (userData.rotation!=undefined)? userData.rotation: 0;
+		// if (!userData.position) return;
+		// var position = userData.position;
+		// var rotation = (userData.rotation!=undefined)? userData.rotation: 0;
 		
-		MyAvatar.orientation = Quat.fromPitchYawRollDegrees(0, rotation, 0);
-		MyAvatar.position = Vec3.sum(
-			Vec3.subtract(MyAvatar.position, MyAvatar.getWorldFeetPosition()),
-			position
-		);
+		// MyAvatar.orientation = Quat.fromPitchYawRollDegrees(0, rotation, 0);
+		// MyAvatar.position = Vec3.sum(
+		// 	Vec3.subtract(MyAvatar.position, MyAvatar.getWorldFeetPosition()),
+		// 	position
+		// );
+
+		if (!userData.address) return;
+		Window.location = userData.address;
 	};
 })
