6b337ba887961ecf76ad842fc810f02a8bf23a8e
Maki <mxmcube@gmail.com>
2019-04-10 19:10:58 -0700

    Updates

--- client-entity-scripts/shaderScreen/screen.fs
+++ client-entity-scripts/shaderScreen/screen.fs
@@ -23,14 +23,14 @@ float getProceduralFragment(inout ProceduralFragment frag) {
 		// crop height
 		float screenSize = height/width;
 		float borderSize = (1-screenSize)/2; 
-		if (uv.y > 1-borderSize) return;
-		if (uv.y < borderSize) return;
+		if (uv.y > 1-borderSize) discard;
+		if (uv.y < borderSize) discard;
 	} else {
 		// crop width
 		float screenSize = width/height;
 		float borderSize = (1-screenSize)/2;
-		if (uv.x > 1-borderSize) return;
-		if (uv.x < borderSize) return;
+		if (uv.x > 1-borderSize) discard;
+		if (uv.x < borderSize) discard;
 	}
 
 	vec3 color = texture(iChannel0, uv).rgb;
