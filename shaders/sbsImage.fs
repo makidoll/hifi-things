d60d31d25c6614f53f50a5afabdc795a553b175f
Maki <mxmcube@gmail.com>
2019-04-14 13:05:46 -0700

    SBS image

--- shaders/sbsImage.fs
+++ shaders/sbsImage.fs
@@ -1,20 +1,30 @@
 /*
 {
+	"_comment": "channels in order: diffuse, orm, normal",
 	"ProceduralEntity": {
 		"shaderUrl": "https://hifi.maki.cat/shaders/SBSimage.fs",
-		"channels": ["https://cutelab.space/u/V1RdYM.jpg"],
+		"channels": [
+			"https://cutelab.space/u/V1RdYM.jpg"
+		],
 		"uniforms": {
-			"alphaClipping": 0.5,
 			"fullbright": 0,
+			"specular": 0.5,
+			"alphaClipping": 0.5,
+
+			"enableOrm": 0,
+			"enableNormal": 0
 		},
 		"version": 3
 	}
 }
 */
 
-uniform float alphaClipping = 0.5;
 uniform float fullbright = 0;
-uniform float specular = 0;
+uniform float specular = 0.5;
+uniform float alphaClipping = 0.5;
+
+uniform float enableOrm = 0;
+uniform float enableNormal = 0;
 
 float getProceduralFragment(inout ProceduralFragment frag) {
 	vec2 uv = _position.xy+0.5;
@@ -29,12 +39,23 @@ float getProceduralFragment(inout ProceduralFragment frag) {
 	if (fullbright>0.5) {
 		frag.emissive = color;
 		frag.diffuse = vec3(0);
+		frag.occlusion = 0;
+		frag.roughness = 1;
 	} else {
 		frag.diffuse = color;
-	}
+		frag.specular = vec3(specular);
 
-	frag.specular = vec3(0);
-	frag.roughness = 1;
+		if (enableOrm>0.5) {
+			vec3 orm = texture(iChannel1, uv).rgb;
+			frag.occlusion = orm.r;
+			frag.roughness = orm.g;
+			frag.metallic = orm.b;
+		}
+		if (enableNormal>0.5) {
+			vec3 normal = texture(iChannel1, uv).rgb;
+			frag.normal = normal;
+		}
+	}
 
 	return 0;
 }
