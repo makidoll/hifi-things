0211667380ee54ccf549cb2a124b71060e1ce11f
Maki <mxmcube@gmail.com>
2019-04-17 16:38:08 -0700

    Pretty Nametags!

---
+++ client-scripts/makisPrettyNametags/makisPrettyNametags.js
@@ -0,0 +1,216 @@
+var nametags = {};
+
+function getUsers(resolve) {
+	var req = new XMLHttpRequest();
+	req.onreadystatechange = function() {
+		if (req.readyState == 4 && req.status == 200) {
+			var json = req.responseText;
+			try { json = JSON.parse(json); } catch(err) { return; }
+			if (json.status!="success") return;
+			resolve(json.data.users);
+		}
+	}
+	req.open("GET",
+		AccountServices.metaverseServerURL+
+		"/api/v1/users?per_page=1000&status="+
+		location.domainID.slice(1,-1)+
+		"&a="+(new Date().getTime()),
+	true);
+	req.send();
+}
+
+function updateNametagEntity(nametag) {
+	if (nametag.entity==undefined) return;
+
+	Entities.editEntity(nametag.entity, {
+		alpha: 1,
+		imageURL: ("https://maki.cat/prettyNametags"+
+			"?username="+(nametag.username||"anonymous")+
+			"&avatarURL="+nametag.avatarURL+
+			"&connection="+nametag.connection+
+			"&admin="+nametag.admin
+		),
+	});
+}
+
+function addNametag(id, details) {
+	/* details = {
+		username: String,
+		avatarURL: String,
+		connection: String,
+		admin: Boolean,
+	} */
+
+	if (nametags[id]!=undefined) return;
+	var nametag = details;
+
+	var scale = 1.5;
+	nametag.entity = Entities.addEntity({
+		name: "cat.maki.nametags",
+		parentID: id,
+		localPosition: {y:1},
+		dimensions: {x:scale, y:scale/8},
+
+		type: "Image",
+		emissive: true,
+		keepAspectRatio: false,
+		billboardMode: "full",
+		imageURL: "",
+		alpha: 0,
+	}, "local");
+
+	updateNametagEntity(nametag);
+	nametags[id] = nametag;
+}
+
+function updateNametag(nametag, details) {
+	var updated = false;
+
+	function update(key) {
+		if (details[key]==undefined) return;
+		if (nametag[key]!=details[key]) {
+			nametag[key] = details[key];
+			updated = true;
+		}
+	}
+
+	update("username");
+	update("avatarURL");
+	update("connection");
+	update("admin");
+
+	if (updated) updateNametagEntity(nametag);
+}
+
+function updateUserLocal(id) {
+	var localUser = AvatarList.getAvatar(id);
+	var nametag = nametags[id];
+
+	if (localUser.sessionUUID == null) {
+		if (nametag==undefined) return; // doesnt exist
+		// must have left
+		Entities.deleteEntity(nametag.entity);
+		delete nametag;
+		return;
+	}
+
+	if (nametag==undefined) {
+		// add nametag
+		addNametag(id, {
+			username: localUser.displayName,
+			avatarURL: "https://metaverse.highfidelity.com/assets/users/hero-default-user-d5a4727d1ad1fb9d9cd26383e26e2697dfd9f4d2f3f81da86c4990771ca8810d.png",
+			connection: null,
+			admin: false,
+		});
+	} else {
+		// update nametag
+		updateNametag(nametag, {
+			username: localUser.displayName
+		});
+	}
+
+	// check if admin
+	Users.requestUsernameFromID(id);
+}
+
+function updateUsersLocal() {
+	//console.log("Updating users locally");
+
+	AvatarList.getAvatarIdentifiers().forEach(function(id) {
+		if (id==null) return;
+		updateUserLocal(id);
+	});
+}
+
+function updateUsersAPI() {
+	//console.log("Updating users with API");
+
+	getUsers(function(apiUsers) {
+		apiUsers.forEach(function(apiUser) {
+			var id = "{"+apiUser.location.node_id+"}";
+			var nametag = nametags[id];
+			if (nametag==undefined) return;
+
+			updateNametag(nametag, {
+				avatarURL: apiUser.images.thumbnail,
+				connection: apiUser.connection,
+			});
+		});	
+	});
+}
+
+var gracefullyUpdateReady = true;
+var gracefullyUpdateToBeUpdated = false;
+
+function gracefullyUpdateUsersAPI() {
+	if (gracefullyUpdateReady==false) {
+		gracefullyUpdateToBeUpdated = true;
+	} else {
+		updateUsersAPI();
+		gracefullyUpdateReady = false;
+
+		Script.setTimeout(function() {
+			gracefullyUpdateReady = true;
+
+			if (gracefullyUpdateToBeUpdated) {
+				updateUsersAPI();
+				gracefullyUpdateToBeUpdated = false;
+			}
+		}, 1000*5);
+	}
+}
+
+updateUsersLocal();
+gracefullyUpdateUsersAPI();
+
+function avatarAddedEvent(id) {
+	updateUserLocal(id);
+	gracefullyUpdateUsersAPI();
+}
+
+function avatarRemovedEvent(id) {
+	updateUserLocal(id);
+	gracefullyUpdateUsersAPI();
+}
+
+function usernameFromIDReply(id, username, machineFingerprint, isAdmin) {
+	var nametag = nametags[id];
+	if (nametag==undefined) return;
+	updateNametag(nametag, {
+		admin: isAdmin
+	});
+}
+
+// function avatarSessionChangedEvent(id, oldId) {
+// 	updateUsersLocal();
+// 	gracefullyUpdateUsersAPI();
+// }
+
+var updateIntervalLocal = Script.setInterval(function() {
+	updateUsersLocal();
+}, 1000*10);
+
+var updateIntervalAPI = Script.setInterval(function() {
+	gracefullyUpdateUsersAPI();
+}, 1000*20);
+
+AvatarList.avatarAddedEvent.connect(avatarAddedEvent);
+AvatarList.avatarRemovedEvent.connect(avatarRemovedEvent);
+// AvatarList.avatarSessionChangedEvent.connect(avatarSessionChangedEvent);
+
+Users.usernameFromIDReply.connect(usernameFromIDReply);
+
+Script.scriptEnding.connect(function() {
+	Script.clearInterval(updateIntervalLocal);
+	Script.clearInterval(updateIntervalAPI);
+
+	AvatarList.avatarAddedEvent.disconnect(avatarAddedEvent);
+	AvatarList.avatarRemovedEvent.disconnect(avatarRemovedEvent);
+	// AvatarList.avatarSessionChangedEvent.disconnect(avatarSessionChangedEvent);
+
+	Users.usernameFromIDReply.disconnect(usernameFromIDReply);
+
+	Object.keys(nametags).forEach(function(id) {
+		Entities.deleteEntity(nametags[id].entity);
+	});
+});
---
+++ client-scripts/makisPrettyNametags/prettyNametags/app.js
@@ -0,0 +1,143 @@
+var puppeteer = require("puppeteer");
+var express = require("express");
+var mitt = require("mitt");
+var fs = require("fs");
+
+var app = express();
+var events = mitt();
+
+var browserRunning = false;
+
+var cache = {};
+
+var scale = 2;
+var viewport = [
+	512*scale,
+	64*scale
+];	
+
+(async ()=>{
+	const browser = await puppeteer.launch();
+	browserRunning = true;
+	console.log("Browser running");
+
+	events.on("generateNametag", async info=>{ // {id,username,avatarURL,connection,admin}
+		let html = fs.readFileSync(__dirname+"/nametag.html", "utf8");
+
+		html = html.replace(/\[username\]/g, info.username);
+		html = html.replace(/\[avatarURL\]/g, info.avatarURL);
+		switch (info.connection) {
+			case "connection": html = html.replace(/<!--connection([\s\S]*?)-->/g, "$1"); break;
+			case "friend": html = html.replace(/<!--friend([\s\S]*?)-->/g, "$1"); break;
+		}
+		if (info.admin) html = html.replace(/<!--admin([\s\S]*?)-->/g, "$1");
+
+		const page = await browser.newPage();
+		await page.setViewport({
+			width: viewport[0],
+			height: viewport[1],
+			deviceScaleFactor: 1
+		})
+
+		page.on("requestfinished", async ()=>{
+			//const width = await page.$eval("#nametag", el=>el.offsetWidth*4);
+
+			const buffer = await page.screenshot({
+				type: "png",
+				omitBackground: true,
+				clip: {
+					x: 0, y: 0,
+					width: viewport[0],
+					height: viewport[1],
+				}
+			});
+
+			events.emit("generateNametagReceive", {
+				id: info.id,
+				buffer: buffer,
+				//width: width/256,
+			});
+
+			page.close();
+		});
+
+		try { await page.setContent(html); } catch(err) {} 
+	});
+})();
+
+let lastID = 0;
+const generateNametag = info=>new Promise((resolve,reject)=>{
+	/* info = {
+		username: String,
+		avatarURL: String,
+		connection: String,
+		admin: Boolean,
+	} */
+
+	let infoKey = JSON.stringify(info);
+
+	// check cache
+	let cachedBuffer = cache[infoKey];
+	if (cachedBuffer!=undefined) {
+		return resolve(cachedBuffer);
+	}
+
+	// generate id for the event handler
+	let id = lastID;
+	lastID++;
+	if (lastID>10000) lastID = 0;
+
+	function receive(info) { // {id,buffer}
+		if (info.id != id) return;
+		resolve(info.buffer);
+		cache[infoKey] = info.buffer;
+		events.off("generateNametagReceive", receive);
+		return;
+	}
+
+	events.on("generateNametagReceive", receive);
+
+	info.id = id;
+	events.emit("generateNametag", info);
+});
+
+app.get("/", (req,res)=>{
+	if (req.query.username==undefined) return res.end();
+	if (req.query.avatarURL==undefined) return res.end();
+	if (req.query.connection==undefined) return res.end();
+	if (req.query.admin==undefined) return res.end();
+
+	function completeReq() {
+		//console.log("Generating "+req.query.username);
+		generateNametag({
+			username: req.query.username,
+			avatarURL: req.query.avatarURL.split("?")[0],
+			connection: req.query.connection,
+			admin: req.query.admin!="false",
+		}).then(buffer=>{
+			res.setHeader("Content-Type", "image/png");
+			//res.setHeader("Nametag-Width", nametag.width)
+			res.end(buffer);
+		}).catch(err=>{});
+	}
+
+	if (!browserRunning) {
+		let interval = setInterval(()=>{
+			if (!browserRunning) return;
+			clearInterval(interval);
+			completeReq();
+		}, 400);
+	} else {
+		completeReq();
+	}
+});
+
+app.listen(8080);
+
+/*
+http://127.0.0.1:8080
+?username=Maki
+&connection=friend
+&admin=true
+&avatarURL=https://hifi-metaverse.s3-us-west-1.amazonaws.com/images/users/previews/323/de9/46-/thumbnail/hifi-user-323de946-477d-468b-a32a-0b4929fb1d11.png
+*/
